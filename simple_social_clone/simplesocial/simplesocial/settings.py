1dbd2175ce18dff3a601164e6322208e16ffcad0
Dheeraj Gopalakrishnan <gopalakrishnandheeraj@gmail.com>
2019-04-27 14:17:12 +1200

    Debuged and completed rest of application

--- simple_social_clone/simplesocial/simplesocial/settings.py
+++ simple_social_clone/simplesocial/simplesocial/settings.py
@@ -37,8 +37,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.humanize',
     'bootstrap4',
-    'accounts',
+    'accounts.apps.AccountsConfig',
+    'groups.apps.GroupsConfig',
+    'posts.apps.PostsConfig',
 ]
 
 MIDDLEWARE = [
@@ -120,7 +123,7 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
-STATICFILES_DIR = [os.path.join(BASE_DIR,'static')]
+STATICFILES_DIRS = [os.path.join(BASE_DIR,'static')]
 
 LOGIN_REDIRECT_URL = 'test'
 LOGOUT_REDIRECT_URL = 'thanks'
